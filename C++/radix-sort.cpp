c9361d6a359ce179cf3e82b6c7dfadc4cc57391d
byronncat <anhthinhncat@gmail.com>
2026-06-11 01:35:48 +0700

    radix sort

---
+++ C++/radix-sort.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int getMax(vector<int> &arr)
+{
+    int mx = arr[0];
+    for (int i = 1; i < arr.size(); i++)
+        if (arr[i] > mx)
+            mx = arr[i];
+    return mx;
+}
+
+void countingSort(vector<int> &arr, int exp)
+{
+    int n = arr.size();
+    vector<int> output(n);
+    int count[10] = {0};
+
+    // Count occurrences of digits
+    for (int i = 0; i < n; i++)
+        count[(arr[i] / exp) % 10]++;
+
+    // Change count[i] so that it now contains the actual position of this digit in output[]
+    for (int i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+
+    // Build the output array
+    for (int i = n - 1; i >= 0; i--)
+    {
+        int digit = (arr[i] / exp) % 10;
+        output[count[digit] - 1] = arr[i];
+        count[digit]--;
+    }
+
+    // Copy the output array back to arr
+    for (int i = 0; i < n; i++)
+        arr[i] = output[i];
+}
+
+void radixSort(vector<int> &arr)
+{
+    int m = getMax(arr);
+
+    for (int exp = 1; m / exp > 0; exp *= 10)
+        countingSort(arr, exp);
+}
+
+int main()
+{
+    vector<int> arr = {170, 45, 75, 90, 2, 802, 100};
+    radixSort(arr);
+    for (int i = 0; i < arr.size(); i++)
+        cout << arr[i] << " ";
+    return 0;
+}
